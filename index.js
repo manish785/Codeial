d6aefabd73517fc86b6380f073480a246101c6c2
manish785 <kumarachilish1997@gmail.com>
2023-04-17 13:16:10 +0530

    set up the directory and fire up the Express server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+
+
+
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running server: ${err}`);
+        return;
+    }
+    console.log(`Server is running on port: ${port}`)
+})
