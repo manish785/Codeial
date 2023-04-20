62e4da20a1ecacde3c898544361207c7e190d884
manish785 <kumarachilish1997@gmail.com>
2023-04-20 14:05:28 +0530

    refactored the code, little messed up in the code

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
 const router = express.Router();
+const homeController = require('../controllers/home_controller');
 
-//accessing the home controller
-const homeController = require('../controllers/home_controller'); 
 console.log('router loaded');
 
-//this request will go and the access the home function in the home controller
+
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
 
 module.exports = router;
