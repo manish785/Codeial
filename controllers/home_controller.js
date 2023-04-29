c9f70d871d206e6d9538f386c6d01e7fa361a3ff
manish785 <kumarachilish1997@gmail.com>
2023-04-29 13:58:45 +0530

    display all the posts realted to particular user and applied authentication for creating post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,30 @@
+const Post = require('../models/post');
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({})
+    // .then(posts => {
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // })
+    // .catch(err => {
+    //   console.log(err);
+    // });
+
+    // fetch all the posts of each user
+    Post.find({}).populate('user').exec()
+    .then(posts => {
+        console.log(posts)
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts,
+        });
+    })
+    .catch(err => {
+        console.log('error', err);
     });
 }
 
