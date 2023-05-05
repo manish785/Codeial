bb3f3ec94f6544d2d078bfd4ed779ce4ddb97734
manish785 <kumarachilish1997@gmail.com>
2023-05-05 13:35:07 +0530

    Distributed the Code into the Partials and generated User's Profile link

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
@@ -14,7 +15,7 @@ module.exports.home = function(req, res){
     //   console.log(err);
     // });
 
-    // fetch all the posts of each user
+    // fetch all the  posts of each user
     Post.find({})
     .populate('user')
     .populate({
@@ -25,11 +26,21 @@ module.exports.home = function(req, res){
     })
     .exec()
     .then(posts => {
-        console.log(posts)
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts: posts,
+        
+        User.find({})
+        .then((users) => {
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts: posts,
+                all_users: users
+            });
+        })
+        .catch((err) => {
+            console.log('error', err);
         });
+    
+
+
     })
     .catch(err => {
         console.log('error', err);
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 const usersController = require('../controllers/users_controller');
 
 // In the users Controller, we are accessing the profile method
-router.get('/profile',passport.checkAuthentication,  usersController.profile);
+router.get('/profile/:id',passport.checkAuthentication,  usersController.profile);
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
