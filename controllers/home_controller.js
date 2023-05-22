2694aa5798789dae67e3950a5233856e910116e7
manish785 <kumarachilish1997@gmail.com>
2023-05-22 12:53:19 +0530

    converted the code into AJAX and added file upload feature

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -18,6 +18,7 @@ module.exports.home = async function(req, res){
     // fetch all the  posts of each user
     try{
         let posts = await Post.find({})
+        .sort('-createdAt')
         .populate('user')
         .populate({
             path: 'comments',
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,10 +3,20 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
     try{
-        await Post.create({
+        let post = await Post.create({
           content: req.body.content,
           user: req.user._id,
       })
+
+      if(req.xhr){
+        return res.status(200).json({
+          data:{
+            post: post
+          },
+          message : 'Post created!'
+        });
+      }
+
       req.flash('success', 'Post created successfully');
       return res.redirect('back');
     }catch(err){
@@ -25,6 +35,17 @@ module.exports.destroy = async function(req, res){
         postInstance.deleteOne();
   
         await Comment.deleteMany({ post: req.params.id });
+
+
+       if(req.xhr){
+         return res.status(200).json({
+           data:{
+             post_id: req.params.id
+           },
+           message: 'Post deleted',
+         })
+       }
+
 
         req.flash('success', 'Post and associated comments deleted!');
 
