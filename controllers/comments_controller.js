13fa72ae7d35edfc41f6436a5772d41f20eaeb24
manish785 <kumarachilish1997@gmail.com>
2023-06-11 16:26:54 +0530

    added kue, mailers, workers and created routes, controllers and models for Like

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,9 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
-const commentsMailer = require('../mailers/comments_mailer')
+const commentsMailer = require('../mailers/comments_mailer');
+const commentEmailWorker = require('../workers/comment_email_worker');
+const queue = require('../config/kue');
+
 
 module.exports.create = async function(req, res){
     // try{
@@ -46,8 +49,15 @@ module.exports.create = async function(req, res){
     
         comment = await comment.populate({ path: 'user', select: 'name email' });
     
-        commentsMailer.newComment(comment);
-    
+       // commentsMailer.newComment(comment);
+        let job = queue.create('emails', comment).save(function(err){
+          if(err){
+            console.log('error in sending to the queue', err);
+            return;
+          }
+
+          console.log('job enqueued', job.id);
+        });
         if (req.xhr) {
           return res.status(200).json({
             data: {
--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -3,25 +3,25 @@ const nodeMailer = require('../config/nodemailer');
 
 // this is another way of exporting a method
 exports.newComment = (comment) =>{
-    console.log('inside new Comment mailer');
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from: 'kumarachilish1997@gmail.com',
         //to: comment.user.email,
         to:  'kumarachilish1997@gmail.com',
         subject: "New Comment Published!",
-        html: '<h1>YUP, your comment is now published!</h1>',
-        // auth: {
-        //     user: 'kumarachilish1997@gmail.com', // Update with your Gmail address
-        //     pass: 'achilish0704', // Update with the generated application-specific password
-        //   },
+        html:  htmlString,
+        auth: {
+            user: 'kumarachilish1997@gmail.com', // Update with your Gmail address
+            pass: 'lkxdkeooubzrrydb', // Update with the generated application-specific password
+          },
     },(err, info)=>{
         if(err){
             console.log('error is  sending email', err);
             return;
         }
 
-        console.log('Message sent', info);
+       // console.log('Message sent', info);
         return;
     });
 }
